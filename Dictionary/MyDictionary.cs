3c1688d423cbdfd9d335aba202a811dfb367cca1
Buğçe Korkmaz <bugce2497@gmail.com>
2021-03-07 16:04:30 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T,K>
+    {
+        T[] tKey;
+        K[] tValue;
+        public MyDictionary()
+        {
+            tKey = new T[0];
+            tValue = new K[0];
+        }
+        public void Add(T key, K value)
+        {
+            T[] tempKey = tKey;
+            K[] tempValue = tValue;
+            tKey = new T[tKey.Length + 1];
+            tValue = new K[tValue.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                tKey[i] = tempKey[i];
+                tValue[i] = tempValue[i];
+            }
+            tKey[tKey.Length - 1] = key;
+            tValue[tKey.Length - 1] = value;
+        }
+        public T[] Key
+        {
+            get{ return tKey; }
+        }
+        public K[] Value
+        {
+            get { return tValue; }
+        }
+        public int Length
+        {
+            get{ return tKey.Length; }
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("Table","Masa");
+            myDictionary.Add("Pencil", "Kalem");
+            myDictionary.Add("Chair", "Sandalye");
+            Console.WriteLine(myDictionary.Length);
+            Console.WriteLine("*******************");
+            foreach (var item in myDictionary.Key)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("*******************");
+            foreach (var item in myDictionary.Value)
+            {
+                Console.WriteLine(item);
+            }
+           
+        }
+    }
+}
